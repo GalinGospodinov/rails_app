f34944315b23ca9cd7a43af505d1fb079ae5b33e
pecabum <galin.gospodinov@158ltd.com>
2015-12-11 17:32:50 +0200

    change background image

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   get 'pages/about'
 
-  devise_for :users
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
-
+  root "pages#home", page: "home"
+  devise_for :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
